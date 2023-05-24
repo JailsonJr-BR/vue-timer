e699b67a190af7301b20add35211eed03cf6ea47
JailsonJr-BR <jailsonjunior513@gmail.com>
2023-05-24 00:53:08 -0300

    feat: add Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar">
+    <img src="@/assets/vue.svg" alt="" />
+    <div class="navbar-menu">
+      <PhTimer :size="32" class="navbar-icon-active"></PhTimer>
+      <PhPaperPlane :size="32"></PhPaperPlane>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { PhPaperPlane, PhTimer } from "@phosphor-icons/vue";
+</script>
+
+<style scoped lang="scss">
+@import "@/scss/modules/Navbar.module.scss";
+</style>
